09ed24ffa7c4215afb3220371f129c3129679f54
Abidur Niloy <niloyabidur@gmail.com>
2023-08-03 12:54:41 +0600

    marge sort

---
+++ Practice/practices 3.5/Merge_Sorted_Array.cpp
@@ -0,0 +1,36 @@
+/*
+88. Merge Sorted Array
+https://leetcode.com/problems/merge-sorted-array/
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int mergedSize = m + n;
+        int i = m - 1;
+        int j = n - 1;
+        int k = mergedSize - 1;
+
+        // Merge two arrays while both have elements
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // If there are remaining elements in nums2, copy them to nums1
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+};
+
+
+
+*/

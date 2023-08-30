bbf8aa70397310e6313f5773d87399364348ac40
Abidur Niloy <niloyabidur@gmail.com>
2023-08-30 13:49:42 +0600

    kruscals algorithms

---
+++ Module/graph/MST/kruscals.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Edge
+{
+public:
+    int a, b, w;
+    Edge(int a, int b, int w)
+    {
+        this->a = a;
+        this->b = b;
+        this->w = w;
+    }
+};
+int main()
+{
+    
+    return 0;
+}
